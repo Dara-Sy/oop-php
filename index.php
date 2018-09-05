3f6c73bf26e2d15004fbb1efd582428bcaabdc51
Dara-Sy <darajanne@gmail.com>
2018-09-05 01:23:42 -0400

    committttt

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+    include 'includedir/parentclass.inc.php';
+    include 'includedir/newclass.inc.php';
+    $object = new NewClass;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Object Oriented Programming PHP</title>
+</head>
+<body>
+
+<?php
+    echo $object->info;
+?>
+
+</body>
+</html>
